ea9cefbc57656a945edc60746fd0d637f21af87a
AndriusMatulionis <89296933+AndriusMatulionis@users.noreply.github.com>
2021-11-14 11:18:21 +0200

    pakeitimas

--- filmai.py
+++ filmai.py
@@ -2,25 +2,31 @@ from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 from selenium.common.exceptions import NoSuchElementException
 
-driver = webdriver.Chrome('C:/Users/Andrius/Desktop/filmai_in/Filmai.in-Bot/chromedriver.exe')
+driver = webdriver.Chrome('chromedriver.exe')
 url = ('https://filmai.in/')
 driver.get(url)
+driver.implicitly_wait(5)
 driver.maximize_window()
 
 
 driver.find_element_by_xpath('//*[@id="navbarsExampleContainer"]/div/button[2]').click()
-driver.implicitly_wait(3)
-driver.find_element_by_xpath('//*[@id="login_append"]').send_keys('####')
-driver.find_element_by_xpath('//*[@id="password_append"]').send_keys('####')
+driver.implicitly_wait(5)
+driver.find_element_by_xpath('//*[@id="login_append"]').send_keys('Vaida90')
+driver.implicitly_wait(5)
+driver.find_element_by_xpath('//*[@id="password_append"]').send_keys('Adele0627')
+driver.implicitly_wait(5)
 driver.find_element_by_xpath('//*[@id="rememberThis"]').click()
+driver.implicitly_wait(5)
 driver.find_element_by_xpath('//*[@id="submit_button"]').click()
 
-driver.implicitly_wait(3)
+
+driver.implicitly_wait(5)
 
 try:
     driver.find_element_by_xpath('//*[@id="navCollapseB"]/div/div/div/div[2]/div[1]/div[1]/div/div[2]').click()
+    driver.implicitly_wait(5)
     driver.close()
 except NoSuchElementException:
     driver.close()   
 
-  
+  asdasdasdas
